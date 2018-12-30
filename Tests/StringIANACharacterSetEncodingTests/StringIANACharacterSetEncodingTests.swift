99cb2086ee8a191cd878b8ed4ec3d499f3b4ca83
Tyler Anger <tyler@tylers-macbook-pro.local>
2018-12-30 15:33:21 -0500

    Updated the test to use the new init and property fields

--- Tests/StringIANACharacterSetEncodingTests/StringIANACharacterSetEncodingTests.swift
+++ Tests/StringIANACharacterSetEncodingTests/StringIANACharacterSetEncodingTests.swift
@@ -2,14 +2,20 @@ import XCTest
 @testable import StringIANACharacterSetEncoding
 
 final class StringIANACharacterSetEncodingTests: XCTestCase {
+    let commonNames: [String] = ["UTF8", "UTF-8", "UTF16", "UTF-16", "UNICODE", "ASCII","Windows-1250","Windows1250"]
     func testCommonEncodingNames() {
-        let commonNames: [String] = ["UTF8", "UTF-8", "UTF16", "UTF-16", "UNICODE", "ASCII"]
         for name in commonNames {
-            guard let encoding = String.Encoding(charSetName: name) else {
+            guard let encoding = String.Encoding(IANACharSetName: name) else {
                 XCTFail("Could not find encoding with name '\(name)'")
                 continue
             }
-            print("('\(name)', '\(encoding)'): \(encoding.rawValue) --- [\(encoding.charSetName), \(encoding.noDashCharSetName)]")
+            var ln: String = "'\(name)' { Encoding Value: \(encoding.rawValue), Encoding: '\(encoding)'"
+            if let n = encoding.IANACharSetName { ln += ", IANA Char Name: '\(n)'" }
+            else { ln += ", IANA Char Name: nil" }
+            if let n = encoding.noDashIANACharSetName { ln += ", No Dash IANA Char Name: '\(n)'" }
+            else { ln += ", No Dash IANA Char Name: nil" }
+            ln += " }"
+            print(ln)
         }
     }
     
