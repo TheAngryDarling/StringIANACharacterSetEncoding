c4aeaac58d5c25dc7a9f81226033123bf8508b1a
Tyler Anger <tyler@tylers-macbook-pro.local>
2018-12-30 15:27:21 -0500

    Added specific Package.swift versions for Swift 4.0 and Swift 4.1 so that when compiling on a version that does not have the Objective C runtime it will include the SR_5986 patch This issue was fixed within Swift 4.2 so the patch is not required from that point on This patch is to fix support for the package to run on Linux

---
+++ Package@swift-4.0.swift
@@ -0,0 +1,33 @@
+// swift-tools-version:4.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+var targets: [Target] = []
+
+#if !_runtime(_ObjC)
+//Only setup SR_5986 patch when we are not on in the objective C runtime
+targets.append(.target(name:"SR_5986", dependencies:[]))
+targets.append(.target(name: "StringIANACharacterSetEncoding", dependencies: ["SR_5986"]))
+#else
+targets.append(.target(name: "StringIANACharacterSetEncoding", dependencies: []))
+#endif
+targets.append(.testTarget(name: "StringIANACharacterSetEncodingTests",
+                           dependencies: ["StringIANACharacterSetEncoding"]))
+
+
+let package = Package(
+    name: "StringIANACharacterSetEncoding",
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "StringIANACharacterSetEncoding",
+            targets: ["StringIANACharacterSetEncoding"]),
+        ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
+    targets: targets
+)
+
